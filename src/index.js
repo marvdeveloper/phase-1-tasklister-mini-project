653de4cf586514d53e7ede87bc78d754716da315
Marvin Kadoyo <kadoyomarvin@gmail.com>
2025-03-21 22:14:04 +0300

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const taskDescription = document.getElementById("new-task-description").value.trim();
+
+    if (taskDescription !== "") {
+      const newTask = document.createElement("li");
+      newTask.textContent = taskDescription; 
+
+      taskList.appendChild(newTask);
+    }
+
+    document.getElementById("new-task-description").value = ""; 
+  });
 });
